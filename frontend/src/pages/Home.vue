a993c0ca3bdba94bedca71325c219ddd215fcb9d
iguanya <georgezmwanchez1996@gmail.com>
2024-03-19 17:55:32 +0300

    Added frontend

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="max-w-3xl py-12 mx-auto">
+    <h2 class="font-bold text-lg text-gray-600 mb-4">
+      Welcome {{ session.user }}!
+    </h2>
+	<ul>
+		Hello!
+		{{action.data}}
+	</ul>
+
+  </div>
+</template>
+
+<script setup>
+import { createListResource } from 'frappe-ui'
+import { session } from '../data/session'
+
+const action = createListResource ({
+	doctype: 'Category description',
+	fields: ["*"],
+	limit: 10
+
+})
+action.reload
+
+</script>
