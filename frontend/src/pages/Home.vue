b00282016ea488ecbf4a3f902272e8fc2f5d0195
iguanya <georgezmwanchez1996@gmail.com>
2024-03-19 20:20:10 +0300

    Edited Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,26 +1,62 @@
 <template>
-  <div class="max-w-3xl py-12 mx-auto">
-    <h2 class="font-bold text-lg text-gray-600 mb-4">
-      Welcome {{ session.user }}!
-    </h2>
-	<ul>
-		Hello!
-		{{action.data}}
-	</ul>
+	<div class="max-w-3xl my-20 py-12 mx-auto">
+	  <h2 class="font-bold text-lg text-gray-600 mb-4">
+		Welcome {{ session.user }}!
+	  </h2>
+	  <!-- <ul>
+		<li v-for="item in action.data" :key="item.name">{{ item.name }} - {{ item.modified}}</li>
+	  </ul> -->
+	  <div class="flex flex-row items-centre justify-between">
+	  <h2 class="text-5xl font-bold text-gray-900">Lists</h2>
+	  <button icon-left="plus">New List</button>
+	  </div>
 
-  </div>
-</template>
+	  <div class="mt-2">
+		<card title= "General">
+			<ul>
+				<li class="flex flex-row space-y-2 items-centre justify-between" v-for="item in action.data" :key="item.name">
+					<span>{{ item.name }} - {{ item.category}}</span>
+					<Button @click="completeAction(action.name)" icon="check"/>
+				</li>
+			</ul>
+		</card>
+	  </div>
+	</div>
+  </template>
 
-<script setup>
-import { createListResource } from 'frappe-ui'
-import { session } from '../data/session'
+  <script setup>
+  import { ref } from 'vue'
+  import { createListResource, Card } from 'frappe-ui'
+  import { session } from '../data/session'
 
-const action = createListResource ({
-	doctype: 'Category description',
+  const action = createListResource ({
+	doctype: 'Action',
 	fields: ["*"],
 	limit: 10
+  })
 
-})
-action.reload
+  // Define a reactive variable to store the data
+  const actionData = ref([])
 
-</script>
+  // Function to load data
+  const loadData = async () => {
+	// Await the data loading
+	await action.reload()
+	// Update the reactive variable with fetched data
+	actionData.value = action.data
+  }
+
+  // Call the function to load data
+  loadData()
+
+  const completeAction = async (actionName) => {
+	try {
+	  await action.setValue.submit({
+		name: actionName,
+		category: 'Teaching'
+	  })
+	} catch (error) {
+	  console.error(error)
+	}}
+  </script>
+
