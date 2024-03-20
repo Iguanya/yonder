f40139c33ddcbf5cfe1e1427c4f559e14312745d
iguanya <georgezmwanchez1996@gmail.com>
2024-03-20 10:44:03 +0300

    further update

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -8,26 +8,78 @@
 	  </ul> -->
 	  <div class="flex flex-row items-centre justify-between">
 	  <h2 class="text-5xl font-bold text-gray-900">Lists</h2>
-	  <button icon-left="plus">New List</button>
+	  <Button icon-left="plus">New List</Button>
 	  </div>
 
 	  <div class="mt-2">
 		<card title= "General">
+			<div>
 			<ul>
 				<li class="flex flex-row space-y-2 items-centre justify-between" v-for="item in action.data" :key="item.name">
 					<span>{{ item.name }} - {{ item.category}}</span>
 					<Button @click="completeAction(action.name)" icon="check"/>
 				</li>
 			</ul>
+			<Button @click="dialog1 = true">
+			Add Action
+			</Button>
+			<Dialog
+			:options="{
+				title: 'Confirm',
+				message: 'Are you sure you want to confirm this action?',
+				size: 'xl',
+				actions: [
+				{
+					label: 'Confirm',
+					variant: 'solid',
+					onClick: () => {
+							},
+				},
+				],
+			}"
+			v-model="dialog1"
+			/>
+		</div>
 		</card>
+		<div>
+			<card title="Upload images">
+				<Story :layout="{ width: 500, type: 'grid' }" autoPropsDisabled>
+					<FileUploader
+					:fileTypes="['image/*']"
+					:validateFile="validateFileFunction"
+					@success="onSuccess"
+					>
+					<template
+						v-slot="{
+						file,
+						uploading,
+						progress,
+						uploaded,
+						message,
+						error,
+						total,
+						success,
+						openFileSelector,
+						}"
+					>
+					<Button @click="openFileSelector" :loading="uploading">
+						{{ uploading ? `Uploading ${progress}%` : 'Upload Image' }}
+						</Button>
+					</template>
+					</FileUploader>
+				</Story>
+			</card>
+		</div>
+
 	  </div>
 	</div>
   </template>
 
   <script setup>
   import { ref } from 'vue'
-  import { createListResource, Card } from 'frappe-ui'
+  import {FileUploader, Dialog, createListResource, Card } from 'frappe-ui'
   import { session } from '../data/session'
+
 
   const action = createListResource ({
 	doctype: 'Action',
@@ -37,6 +89,10 @@
 
   // Define a reactive variable to store the data
   const actionData = ref([])
+  const dialog1 = ref(false)
+  const dialog2 = ref(false)
+  const validateFileFunction = (fileObject) => {}
+  const onSuccess = (file) => {}
 
   // Function to load data
   const loadData = async () => {
